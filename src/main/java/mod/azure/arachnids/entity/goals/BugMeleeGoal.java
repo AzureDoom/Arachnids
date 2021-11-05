db2c9f0cebaadb1252e947ac78c2496d1d7ebaf7
AzureDoom <jerry99666@gmail.com>
2021-11-05 15:23:13 -0400

    Update BugMeleeGoal.java

--- src/main/java/mod/azure/arachnids/entity/goals/BugMeleeGoal.java
+++ src/main/java/mod/azure/arachnids/entity/goals/BugMeleeGoal.java
@@ -8,28 +8,16 @@ import net.minecraft.entity.ai.goal.MeleeAttackGoal;
 
 public class BugMeleeGoal extends MeleeAttackGoal {
 	private final BaseBugEntity actor;
-	private int updateCountdownTicks;
 	private final double speed;
-	private int ticksUntilNextAttack;
 	public int cooldown;
-	private double targetX;
-	private double targetY;
-	private double targetZ;
-	private final boolean pauseWhenMobIdle;
 
 	public BugMeleeGoal(BaseBugEntity zombie, double speed) {
 		super(zombie, speed, false);
 		this.actor = zombie;
 		this.speed = speed;
-		this.pauseWhenMobIdle = false;
 	}
 
-	public void start() {
-		super.start();
-		this.updateCountdownTicks = 0;
-		this.cooldown = 0;
-	}
-
+	@Override
 	public void stop() {
 		super.stop();
 		this.actor.setAttacking(false);
@@ -37,44 +25,9 @@ public class BugMeleeGoal extends MeleeAttackGoal {
 	}
 
 	@Override
-	public boolean shouldContinue() {
-		return super.shouldContinue();
-	}
-
-	public void tick() {
-		LivingEntity livingentity = this.actor.getTarget();
-		if (livingentity != null) {
-			this.mob.getLookControl().lookAt(livingentity, 30.0F, 30.0F);
-			double d0 = this.mob.squaredDistanceTo(livingentity.getX(), livingentity.getY(), livingentity.getZ());
-			this.updateCountdownTicks = Math.max(this.updateCountdownTicks - 1, 0);
-			if ((this.pauseWhenMobIdle || this.mob.getVisibilityCache().canSee(livingentity))
-					&& this.ticksUntilNextAttack <= 0
-					&& (this.targetX == 0.0D && this.targetY == 0.0D && this.targetZ == 0.0D
-							|| livingentity.squaredDistanceTo(this.targetX, this.targetY, this.targetZ) >= 1.0D
-							|| this.mob.getRandom().nextFloat() < 0.05F)) {
-				this.targetX = livingentity.getX();
-				this.targetY = livingentity.getY();
-				this.targetZ = livingentity.getZ();
-				this.updateCountdownTicks = 4 + this.mob.getRandom().nextInt(7);
-				if (d0 > 1024.0D) {
-					this.updateCountdownTicks += 10;
-				} else if (d0 > 256.0D) {
-					this.updateCountdownTicks += 5;
-				}
-
-				if (!this.mob.getNavigation().startMovingTo(livingentity, this.speed)) {
-					this.updateCountdownTicks += 15;
-				}
-			}
-			this.ticksUntilNextAttack = Math.max(this.ticksUntilNextAttack - 0, 0);
-			this.attack(livingentity, d0);
-		}
-	}
-
-	@Override
 	protected void attack(LivingEntity livingentity, double squaredDistance) {
 		double d0 = this.getSquaredMaxAttackDistance(livingentity);
-		if (squaredDistance <= d0 && this.getCooldown() <= 10) {
+		if (squaredDistance <= d0 && this.getCooldown() <= 150) {
 			this.resetCooldown();
 			SplittableRandom random = new SplittableRandom();
 			int var = random.nextInt(0, 4);
@@ -95,12 +48,12 @@ public class BugMeleeGoal extends MeleeAttackGoal {
 
 	@Override
 	protected int getMaxCooldown() {
-		return 150;
+		return 400;
 	}
 
 	@Override
 	protected void resetCooldown() {
-		this.cooldown = 0;
+		this.cooldown = -400;
 	}
 
 	@Override
