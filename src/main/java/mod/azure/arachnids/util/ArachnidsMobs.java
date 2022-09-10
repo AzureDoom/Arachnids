ab6d8be5a00b9dea272ed757a07ab42af00d0ac8
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2022-09-10 12:16:49 -0400

    v0.0.5

--- src/main/java/mod/azure/arachnids/util/ArachnidsMobs.java
+++ src/main/java/mod/azure/arachnids/util/ArachnidsMobs.java
@@ -23,56 +23,56 @@ public class ArachnidsMobs {
 	public static final EntityType<MZ90BlockEntity> MZ90 = Registry.register(Registry.ENTITY_TYPE,
 			new Identifier(ArachnidsMod.MODID, "mz90_block"),
 			FabricEntityTypeBuilder.<MZ90BlockEntity>create(SpawnGroup.MISC, MZ90BlockEntity::new)
-					.dimensions(EntityDimensions.fixed(0.98F, 0.98F)).trackRangeBlocks(90).trackedUpdateRate(4)
+					.dimensions(EntityDimensions.fixed(0.98F, 0.98F)).trackRangeBlocks(90).trackedUpdateRate(1)
 					.build());
 
 	public static final EntityType<TONBlockEntity> TON = Registry.register(Registry.ENTITY_TYPE,
 			new Identifier(ArachnidsMod.MODID, "ton_block"),
 			FabricEntityTypeBuilder.<TONBlockEntity>create(SpawnGroup.MISC, TONBlockEntity::new)
-					.dimensions(EntityDimensions.fixed(0.98F, 0.98F)).trackRangeBlocks(90).trackedUpdateRate(4)
+					.dimensions(EntityDimensions.fixed(0.98F, 0.98F)).trackRangeBlocks(90).trackedUpdateRate(1)
 					.build());
 
 	public static final EntityType<ArkellianEntity> ARKELLIANBUG = Registry.register(Registry.ENTITY_TYPE,
 			new Identifier(ArachnidsMod.MODID, "arkellian"),
 			FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, ArkellianEntity::new)
-					.dimensions(EntityDimensions.changing(1.1f, 0.85F)).trackRangeBlocks(90).trackedUpdateRate(4)
+					.dimensions(EntityDimensions.changing(1.1f, 0.85F)).trackRangeBlocks(90).trackedUpdateRate(1)
 					.build());
 
 	public static final EntityType<BrainEntity> BRAINBUG = Registry.register(Registry.ENTITY_TYPE,
 			new Identifier(ArachnidsMod.MODID, "brainbug"),
 			FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, BrainEntity::new)
-					.dimensions(EntityDimensions.changing(5.6f, 4.05F)).trackRangeBlocks(90).trackedUpdateRate(4)
+					.dimensions(EntityDimensions.changing(5.6f, 4.05F)).trackRangeBlocks(90).trackedUpdateRate(1)
 					.build());
 
 	public static final EntityType<HopperEntity> HOOPERBUG = Registry.register(Registry.ENTITY_TYPE,
 			new Identifier(ArachnidsMod.MODID, "hopper"),
 			FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, HopperEntity::new)
-					.dimensions(EntityDimensions.fixed(2.3f, 2.15F)).trackRangeBlocks(90).trackedUpdateRate(4).build());
+					.dimensions(EntityDimensions.fixed(2.3f, 2.15F)).trackRangeBlocks(90).trackedUpdateRate(1).build());
 
 	public static final EntityType<PlasmaEntity> PLASMABUG = Registry.register(Registry.ENTITY_TYPE,
 			new Identifier(ArachnidsMod.MODID, "plasma"),
 			FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, PlasmaEntity::new)
-					.dimensions(EntityDimensions.changing(12.05f, 8.25F)).trackRangeBlocks(90).trackedUpdateRate(4)
+					.dimensions(EntityDimensions.changing(12.05f, 8.25F)).trackRangeBlocks(90).trackedUpdateRate(1)
 					.build());
 
 	public static final EntityType<ScorpionEntity> SCORPIONBUG = Registry.register(Registry.ENTITY_TYPE,
 			new Identifier(ArachnidsMod.MODID, "scorpion"),
 			FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, ScorpionEntity::new)
-					.dimensions(EntityDimensions.changing(12.05f, 8.25F)).trackRangeBlocks(90).trackedUpdateRate(4).build());
+					.dimensions(EntityDimensions.changing(12.05f, 8.25F)).trackRangeBlocks(90).trackedUpdateRate(1).build());
 
 	public static final EntityType<TankerEntity> TANKERBUG = Registry.register(Registry.ENTITY_TYPE,
 			new Identifier(ArachnidsMod.MODID, "tanker"),
 			FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, TankerEntity::new)
-					.dimensions(EntityDimensions.changing(12.6f, 4.95F)).trackRangeBlocks(90).trackedUpdateRate(4)
+					.dimensions(EntityDimensions.changing(12.6f, 4.95F)).trackRangeBlocks(90).trackedUpdateRate(1)
 					.build());
 
 	public static final EntityType<WarriorEntity> WARRIORBUG = Registry.register(Registry.ENTITY_TYPE,
 			new Identifier(ArachnidsMod.MODID, "warrior"),
 			FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, WarriorEntity::new)
-					.dimensions(EntityDimensions.fixed(2.3f, 2.15F)).trackRangeBlocks(90).trackedUpdateRate(4).build());
+					.dimensions(EntityDimensions.fixed(2.3f, 2.15F)).trackRangeBlocks(90).trackedUpdateRate(1).build());
 
 	public static final EntityType<WorkerEntity> WORKERBUG = Registry.register(Registry.ENTITY_TYPE,
 			new Identifier(ArachnidsMod.MODID, "worker"),
 			FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, WorkerEntity::new)
-					.dimensions(EntityDimensions.fixed(2.3f, 2.15F)).trackRangeBlocks(90).trackedUpdateRate(4).build());
+					.dimensions(EntityDimensions.fixed(2.3f, 2.15F)).trackRangeBlocks(90).trackedUpdateRate(1).build());
 }
