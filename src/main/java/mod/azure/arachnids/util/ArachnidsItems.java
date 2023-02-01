3a5a4c0e6717839dd192624b0b949b50eec44e1f
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-02-01 03:16:45 -0500

    Moved Spawn Eggs to AzureLib

--- src/main/java/mod/azure/arachnids/util/ArachnidsItems.java
+++ src/main/java/mod/azure/arachnids/util/ArachnidsItems.java
@@ -1,7 +1,6 @@
 package mod.azure.arachnids.util;
 
 import mod.azure.arachnids.ArachnidsMod;
-import mod.azure.arachnids.items.BugSpawnEgg;
 import mod.azure.arachnids.items.MIArmorItem;
 import mod.azure.arachnids.items.ammo.BulletAmmo;
 import mod.azure.arachnids.items.ammo.FlareItem;
@@ -10,6 +9,7 @@ import mod.azure.arachnids.items.ammo.TONItem;
 import mod.azure.arachnids.items.weapons.M55Item;
 import mod.azure.arachnids.items.weapons.MAR1Item;
 import mod.azure.arachnids.items.weapons.MAR2Item;
+import mod.azure.azurelib.items.AzureSpawnEgg;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
@@ -29,22 +29,22 @@ public class ArachnidsItems {
 	public static FlareItem FLARE = item(new FlareItem(), "flare");
 
 	// Spawn Eggs
-	public static BugSpawnEgg ARKELLIANBUG_SPAWN_EGG = item(
-			new BugSpawnEgg(ArachnidsMobs.ARKELLIANBUG, 0x5a001e, 0x770016), "arkellian_spawn_egg");
-	public static BugSpawnEgg BRAINBUG_SPAWN_EGG = item(new BugSpawnEgg(ArachnidsMobs.BRAINBUG, 0x634e33, 0xf8dec1),
+	public static AzureSpawnEgg ARKELLIANBUG_SPAWN_EGG = item(
+			new AzureSpawnEgg(ArachnidsMobs.ARKELLIANBUG, 0x5a001e, 0x770016), "arkellian_spawn_egg");
+	public static AzureSpawnEgg BRAINBUG_SPAWN_EGG = item(new AzureSpawnEgg(ArachnidsMobs.BRAINBUG, 0x634e33, 0xf8dec1),
 			"brainbug_spawn_egg");
-	public static BugSpawnEgg HOOPERBUG_SPAWN_EGG = item(new BugSpawnEgg(ArachnidsMobs.HOOPERBUG, 0x214c0e, 0x6f9e5a),
-			"hooper_spawn_egg");
-	public static BugSpawnEgg PLAMSABUG_SPAWN_EGG = item(new BugSpawnEgg(ArachnidsMobs.PLASMABUG, 0x020408, 0x535a66),
-			"plasma_spawn_egg");
-	public static BugSpawnEgg SCORPIONBUG_SPAWN_EGG = item(
-			new BugSpawnEgg(ArachnidsMobs.SCORPIONBUG, 0x535a66, 0xaa887c), "scorpion_spawn_egg");
-	public static BugSpawnEgg TANKERBUG_SPAWN_EGG = item(new BugSpawnEgg(ArachnidsMobs.TANKERBUG, 0x091a27, 0x96a8b5),
-			"tanker_spawn_egg");
-	public static BugSpawnEgg WARRIORBUG_SPAWN_EGG = item(new BugSpawnEgg(ArachnidsMobs.WARRIORBUG, 0xa6380a, 0xc15224),
-			"warrior_spawn_egg");
-	public static BugSpawnEgg WORKERBUG_SPAWN_EGG = item(new BugSpawnEgg(ArachnidsMobs.WORKERBUG, 0x3b2c80, 0x55469b),
-			"worker_spawn_egg");
+	public static AzureSpawnEgg HOOPERBUG_SPAWN_EGG = item(
+			new AzureSpawnEgg(ArachnidsMobs.HOOPERBUG, 0x214c0e, 0x6f9e5a), "hooper_spawn_egg");
+	public static AzureSpawnEgg PLAMSABUG_SPAWN_EGG = item(
+			new AzureSpawnEgg(ArachnidsMobs.PLASMABUG, 0x020408, 0x535a66), "plasma_spawn_egg");
+	public static AzureSpawnEgg SCORPIONBUG_SPAWN_EGG = item(
+			new AzureSpawnEgg(ArachnidsMobs.SCORPIONBUG, 0x535a66, 0xaa887c), "scorpion_spawn_egg");
+	public static AzureSpawnEgg TANKERBUG_SPAWN_EGG = item(
+			new AzureSpawnEgg(ArachnidsMobs.TANKERBUG, 0x091a27, 0x96a8b5), "tanker_spawn_egg");
+	public static AzureSpawnEgg WARRIORBUG_SPAWN_EGG = item(
+			new AzureSpawnEgg(ArachnidsMobs.WARRIORBUG, 0xa6380a, 0xc15224), "warrior_spawn_egg");
+	public static AzureSpawnEgg WORKERBUG_SPAWN_EGG = item(
+			new AzureSpawnEgg(ArachnidsMobs.WORKERBUG, 0x3b2c80, 0x55469b), "worker_spawn_egg");
 
 	// Armor
 	public static final Item MI_HELMET = item(new MIArmorItem(EquipmentSlot.HEAD), "miarmor_helmet");
